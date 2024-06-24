abc588a48a230dcc0a4d3439b8b7f11c44495cf8
Imron Reviady <imronreviady2@gmail.com>
2024-06-24 15:01:49 +0700

    done tab view with adaptive carousel

---
+++ Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturedTabView.swift
+//  Touchdown
+//
+//  Created by Imron Reviady on 24/06/24.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .previewDevice("iPhone 15 Pro")
+            .background(Color.gray)
+    }
+}
