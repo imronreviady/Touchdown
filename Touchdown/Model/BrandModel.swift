58397a2e21458e730f9ce1e067d9a41ba5e2ef8b
Imron Reviady <imronreviady2@gmail.com>
2024-06-24 18:30:47 +0700

    done horizontal grid layout for brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Imron Reviady on 24/06/24.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
